dc43061496960b159add20b7a8defa574ec35f81
Dhruv Haldar <haldardhruv@gmail.com>
2025-05-15 17:22:55 +0000

    decrease font of Learn More

--- src/components/GamePreviewCard.tsx
+++ src/components/GamePreviewCard.tsx
@@ -38,7 +38,7 @@ const GamePreviewCard = ({ title, description, imageSrc, imageAlt, dataAiHint, l
         </div>
       </CardContent>
       <CardFooter className="p-4 bg-secondary/30">
-        <Button asChild className="w-full font-heading text-base group">
+        <Button asChild className="w-full font-heading text-sm group">
           <Link href={linkHref}>
             Learn More 
             <ArrowRight className="ml-2 h-5 w-5 group-hover:translate-x-1 transition-transform" />
